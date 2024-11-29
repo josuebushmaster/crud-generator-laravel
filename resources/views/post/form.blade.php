82972321470a3990041fcbceb28a0f8301089d94
josuebushmaster <josue.pastil77@gmail.com>
2024-11-28 21:42:37 -0600

    Crud laravel generator con bootstrap

---
+++ resources/views/post/form.blade.php
@@ -0,0 +1,19 @@
+<div class="row padding-1 p-1">
+    <div class="col-md-12">
+        
+        <div class="form-group mb-2 mb20">
+            <label for="title" class="form-label">{{ __('Title') }}</label>
+            <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" value="{{ old('title', $post?->title) }}" id="title" placeholder="Title">
+            {!! $errors->first('title', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+        </div>
+        <div class="form-group mb-2 mb20">
+            <label for="content" class="form-label">{{ __('Content') }}</label>
+            <input type="text" name="content" class="form-control @error('content') is-invalid @enderror" value="{{ old('content', $post?->content) }}" id="content" placeholder="Content">
+            {!! $errors->first('content', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+        </div>
+
+    </div>
+    <div class="col-md-12 mt20 mt-2">
+        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+    </div>
+</div>
